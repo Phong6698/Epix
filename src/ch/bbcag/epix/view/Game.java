119a3cff6dc4fb6f87d5383683889845744c7fcd
zpengc-bbc <chirametphong.penglerd@bbcag.ch>
2015-05-13 11:41:52 +0200

    Signed-off-by: zpengc-bbc <chirametphong.penglerd@bbcag.ch>

--- src/ch/bbcag/epix/view/Game.java
+++ src/ch/bbcag/epix/view/Game.java
@@ -13,11 +13,10 @@ import ch.bbcag.epix.entity.User;
 public class Game {
 	
 	public static void main(String[] args) {
-		
 		User user = new User();
 		user.setUsername("Epix");
-		user.setCoin(100);
-		user.setMaxHealth(100);
+		user.setCoin(0);
+		user.setMaxHealth(50);
 
 		GameFrame game = new GameFrame(1, user);
 		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

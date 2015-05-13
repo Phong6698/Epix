747403ac8c71541fef36ad7516d399fcae6d8867
zpengc-bbc <chirametphong.penglerd@bbcag.ch>
2015-05-13 08:40:07 +0200

    Display update

--- src/ch/bbcag/epix/gamestate/GameStateManager.java
+++ src/ch/bbcag/epix/gamestate/GameStateManager.java
@@ -78,10 +78,10 @@ public class GameStateManager {
 
 			// if finished update coin in database
 			if (getGameStates()[currentLevel].finished) {
-//				EpixController.getInstance().coinsUpdate(getGameStates()[currentLevel].player.getUser(), getGameStates()[currentLevel].player.getCoin());
-//				int level_ID = EpixController.getInstance().getID_Level(getGameStates()[currentLevel].levelName);
-//				EpixController.getInstance().save(user, level_ID);
-//				user.setCoin(getGameStates()[currentLevel].player.getCoin());
+				EpixController.getInstance().coinsUpdate(getGameStates()[currentLevel].player.getUser(), getGameStates()[currentLevel].player.getCoin());
+				int level_ID = EpixController.getInstance().getID_Level(getGameStates()[currentLevel].levelName);
+				EpixController.getInstance().save(user, level_ID);
+				user.setCoin(getGameStates()[currentLevel].player.getCoin());
 				this.setFinished(true);
 			}
 

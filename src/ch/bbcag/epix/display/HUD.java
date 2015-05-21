9eb1a3d4787b2f582fd5aacbd7f611b81f2bdf8c
Chiramet Phong Penglerd <ChirametPhong.Penglerd@bbcag.ch>
2015-05-21 15:06:29 +0200

    HUD Update

--- src/ch/bbcag/epix/display/HUD.java
+++ src/ch/bbcag/epix/display/HUD.java
@@ -1,10 +1,11 @@
-package ch.bbcag.epix.display;
+	package ch.bbcag.epix.display;
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.awt.image.RescaleOp;
 
 import javax.imageio.ImageIO;
 
@@ -41,29 +42,32 @@ public class HUD {
 	}
 
 	public void draw(Graphics2D g) {
-		
-		
-		g.drawImage(hud, 11, 15, null);
+
+		float[] scales = { 1f, 1f, 1f, 0.6f };
+		float[] offsets = new float[4];
+		RescaleOp rop = new RescaleOp(scales, offsets, null);
+
+		g.drawImage(hud, rop, 140, 15); //311 - 171
 		
 		// Player Information
-		g.drawImage(coin, 100, 36, null);
+		g.drawImage(coin, 229, 36, null);
 		g.setFont(font);
 		g.setColor(Color.BLACK);
-		g.drawString(getPlayer().getHealth() + "/" + getPlayer().getMaxHealth(), 51, 31);
-		g.drawString(getPlayer().getUsername(), 20, 47);
-		g.drawString("" + getPlayer().getCoin(), 117, 47);
+		g.drawString(getPlayer().getHealth() + "/" + getPlayer().getMaxHealth(), 180, 31);
+		g.drawString(getPlayer().getUsername(), 149, 47);
+		g.drawString("" + getPlayer().getCoin(), 246, 47);
 
 		// Player Coordinates
-		g.drawString("x: " + getPlayer().getx(), 400, 43);
-		g.drawString("y: " + getPlayer().gety(), 400, 57);
+		g.drawString("x: " + getPlayer().getx(), 20, 43);
+		g.drawString("y: " + getPlayer().gety(), 20, 57);
 
 		// Player health bar
 		g.setColor(new Color(1f, 0f, 0f, .3f));
-		Rectangle backgroundBar = new Rectangle(20, 31, 120, 5);
+		Rectangle backgroundBar = new Rectangle(149, 31, 120, 5);
 		g.fill(backgroundBar);
 
 		g.setColor(Color.RED);
-		Rectangle healthBar = new Rectangle(20, 31, (int) (((float) getPlayer().getHealth() / (float) getPlayer().getMaxHealth()) * 120), 5);
+		Rectangle healthBar = new Rectangle(149, 31, (int) (((float) getPlayer().getHealth() / (float) getPlayer().getMaxHealth()) * 120), 5);
 		g.fill(healthBar);
 
 	}

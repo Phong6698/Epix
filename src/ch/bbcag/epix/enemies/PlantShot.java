7767426aa5a9e0a72e06285a912d338f4fc26fa1
zpengc-bbc <chirametphong.penglerd@bbcag.ch>
2015-05-27 14:33:30 +0200

    Signed-off-by: zpengc-bbc <chirametphong.penglerd@bbcag.ch>

--- src/ch/bbcag/epix/enemies/PlantShot.java
+++ src/ch/bbcag/epix/enemies/PlantShot.java
@@ -123,7 +123,6 @@ public class PlantShot extends MapObject {
 			setRemove(true);
 		}
 		
-		System.out.println("x: " + this.getx() );
 		//loeschen falls es die y koordinate <= 0
 		if (this.gety() <= 0 || this.gety() >= 270 || this.getx() <= 5  || this.getx() >= 3188) {
 			setRemove(true);
